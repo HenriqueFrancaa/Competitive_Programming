56c294a2248e3a92eacd8ef3d3bb75cebb17ab4f
HenriqueFrancaa <henrique23.franca@gmail.com>
2025-01-06 17:20:45 -0300

    odd or even this is the question. solved!

--- codeforces/C++/a.add_odd_or_subtract_even/main.cpp
+++ codeforces/C++/a.add_odd_or_subtract_even/main.cpp
@@ -19,9 +19,11 @@ int main(){
         }
     }
 
-    // se eu tenho A = 3 e B = 7, x = 7 - 3 = 4 e 4 é par, então posso só fazer a = a-4 = 3 = b: total de mov = 1
+    // se eu tenho A = 3 e B = 7, x = 7 - 3 = 4 e 4 é par, então tenho que fazer a = a+(4-1) = 6
+    // e entao x = 7 - 6 = 1 e 1 é impar, e agora somo a = a + 1 = 7 = b e total de mov = 2
     // se eu tenho A = 2 e B = 6, x = 6 - 2 = 4 e 4 é par, mas eu tenho que fazer o A aumentar, então faço a = a+(4-1)
     // a = 5, x = 6-5 = 1 e 1 é impar, então 1+5 = 6 = b: total de mov = 2
+    // se eu tenho A = 6 e B = 2, x = 6-2 = 4 e 4 é par, então faço a = a - 4 = 2 = b e total de mov = 1 
 
     //se eu subtrair (impar - impar) ou (par - par) = par
     //se eu subtrair (par - impar) ou (impar - par) = impar
