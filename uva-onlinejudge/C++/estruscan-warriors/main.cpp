43c6fefaca70dd3ef2f53eea298fbeb86633913a
HenriqueFrancaa <henrique23.franca@gmail.com>
2025-04-22 15:08:50 -0300

    Question solved!

---
+++ uva-onlinejudge/C++/estruscan-warriors/main.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+typedef long long ll;
+
+int main(){
+    ll t; cin >> t;
+    while(t--){
+        ll n; cin >> n;
+        ll  rows = ( -1 + sqrt(1 + 8*n))/2;
+        cout << rows << endl;
+    }
+    return 0;
+    //Uva-OnlineJudge 11614 - Etruscan Warriors Never Play Chess
+}
