29acfbc2acc021bc2c885dc592e1baafe19bb861
HenriqueFrancaa <henrique23.franca@gmail.com>
2025-04-25 17:48:37 -0300

    Question solved

---
+++ uva-onlinejudge/C++/Searching_for_nessy/main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int t;cin >> t;
+    while(t--){
+        int n,m; cin >> n >> m;
+        cout << (n/3)*(m/3) << endl;
+    }
+    return 0;
+    // UvaOnlineJudge - 11044 - Searching for Nessy
+    // https://onlinejudge.org/index.php?option=com_onlinejudge&Itemid=8&page=show_problem&problem=1985
+
+}
