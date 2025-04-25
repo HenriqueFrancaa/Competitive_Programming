b8c4deb94d258941946dec1640a4778ed9b9009a
HenriqueFrancaa <henrique23.franca@gmail.com>
2025-04-25 16:55:58 -0300

    Question solved!

---
+++ uva-onlinejudge/C++/Celebrity_jeopardy/main.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+    string eq;
+    while(getline(cin, eq)){
+        cout << eq << endl;
+        eq.clear();
+    }
+    return 0;
+    // Uva OnlineJudge 01124
+    // https://onlinejudge.org/index.php?option=com_onlinejudge&Itemid=8&page=show_problem&problem=3565
+}
