55f4b66776e6eb9d702bc82b223e7e20cb8e8b8d
HenriqueFrancaa <henrique23.franca@gmail.com>
2025-07-28 12:48:37 -0300

    Questionsolved!

---
+++ uva-onlinejudge/C++/ugly_numbers/main.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){ 
+    // int count = 0;
+    // int n = 1; 
+    // while(count < 1500){
+    //     int n2 = n;
+    //     while(n2 % 5 == 0){n2 /= 5;}
+    //     while(n2 % 3 == 0){n2 /= 3;}
+    //     while(n2 % 2 == 0){n2 /= 2;}
+    //     if(n2 == 1){ count++; cout << count << " " << n << endl; }
+    //     n++;
+    // }
+    // n--;
+    // cout << n << endl;
+    cout << "The 1500'th ugly number is " << 859963392 << "." << endl;
+
+    //136 - Ugly Numbers - UvaOnlineJudge
+    //https://onlinejudge.org/index.php?option=com_onlinejudge&Itemid=8&page=show_problem&problem=72
+    return 0;
+}
